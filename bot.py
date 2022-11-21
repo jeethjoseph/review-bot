ccb8418b25d56c81d6bcb6225b11ed8f144b2922
Jeeth Joseph <jeethjoseph@ada.local>
2022-11-21 18:42:58 +0530

    gitignore

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import requests
 import slack
 import json
-import pandas as pd
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from glom import glom
+import collections
+import time
 
+ts = time.time()
+print(ts)
 
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
@@ -26,7 +28,8 @@ def run_query(query): # A simple function to use requests.post to make the API c
 
 query = """
 {
-  search(query: "org:Partsavatar-Team is:open is:pr sort:created-asc created:>2021-12-01", type: ISSUE, first: 10) {
+  search(query: "org:Partsavatar-Team is:open is:pr sort:created-asc
+  created:>2021-12-01", type: ISSUE, first: 100) {
     issueCount
     edges {
       node {
@@ -38,7 +41,7 @@ query = """
           }
           createdAt
           url
-          reviewRequests(first: 10) {
+          reviewRequests(first: 100) {
             nodes {
               requestedReviewer {
                 ... on User {
@@ -56,22 +59,20 @@ query = """
 result = run_query(query) # Execute the query
 
 issue_count = result['data']['search']['issueCount']
-print(issue_count)
-df_data = pd.json_normalize(result['data']['search']['edges'])
-#filtered_df = df_data[['node.title','node.createdAt','node.url','node.nodes']['node.reviewRequests.nodes']]
+#print(issue_count)
 
-filtered_df = df_data['node.reviewRequests.nodes']
-#filtered_df['reviewer'] = df_data['node.reviewRequests.nodes'].apply(lambda row: glom(row, 'requestedReviewer.login'))
-filtered_df['reviewer'] = df_data['node.reviewRequests.nodes']
-#df = pd.DataFrame(df_data)
-print(filtered_df)
-print(df_data.columns)
-
-
-
-
-
+user_pending_pr_count_map = dict(
+    collections.Counter(
+        [review_requests['requestedReviewer']['login']
+        for edge in result['data']['search']['edges'] 
+        for review_requests in edge['node']['reviewRequests']['nodes']]
+    )
+)
+print(user_pending_pr_count_map)
+print_payload = "Counts of reviews pending on PRs created since Dec 1 2021" + "\n"
+for dev in user_pending_pr_count_map:
+    print_payload+=dev + " : " + str(user_pending_pr_count_map[dev])+"\n"
 
 
 
-client.chat_postMessage(channel='#general',text='Hello World')
+client.chat_postMessage(channel='#general',text=print_payload)
