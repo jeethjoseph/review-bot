110932682f85597e530341fa1cc979eb19743901
Jeeth Joseph <jeethjoseph@ada.local>
2022-11-22 20:49:11 +0530

    Modified to check if delay is more than a day

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import collections
 import time
+from dateutil import parser
+
 
 ts = time.time()
 print(ts)
@@ -28,51 +30,59 @@ def run_query(query): # A simple function to use requests.post to make the API c
 
 query = """
 {
-  search(query: "org:Partsavatar-Team is:open is:pr sort:created-asc
-  created:>2021-12-01", type: ISSUE, first: 100) {
-    issueCount
-    edges {
-      node {
-        ... on PullRequest {
-          number
-          title
-          repository {
-            nameWithOwner
-          }
-          createdAt
-          url
-          reviewRequests(first: 100) {
-            nodes {
-              requestedReviewer {
-                ... on User {
-                  login
+   search(query: "org:Partsavatar-Team is:open is:pr sort:created-asc
+    created:>2022-11-01", type: ISSUE, first: 100) {
+      issueCount
+      edges {
+        node {
+          ... on PullRequest {
+            number
+            title
+            repository {
+              nameWithOwner
+            }
+            createdAt
+            url
+            timelineItems(itemTypes: REVIEW_REQUESTED_EVENT, first: 10) {
+              nodes {
+                ... on ReviewRequestedEvent {
+                  createdAt
+                  
+                  requestedReviewer {
+                    ... on User {
+                      login
+                    }
+                  }
                 }
               }
             }
-          }
         }
       }
-    }
   }
-}
+  }
+  }
 """
 result = run_query(query) # Execute the query
-
-issue_count = result['data']['search']['issueCount']
-#print(issue_count)
 
 user_pending_pr_count_map = dict(
     collections.Counter(
         [review_requests['requestedReviewer']['login']
         for edge in result['data']['search']['edges'] 
-        for review_requests in edge['node']['reviewRequests']['nodes']]
+        for review_requests in edge['node']['timelineItems']['nodes']]
     )
 )
-print(user_pending_pr_count_map)
-print_payload = "Counts of reviews pending on PRs created since Dec 1 2021" + "\n"
+
+
+
+print_payload = "Counts of reviews pending on PRs created since Nov 1 2022" + "\n"
 for dev in user_pending_pr_count_map:
     print_payload+=dev + " : " + str(user_pending_pr_count_map[dev])+"\n"
 
+print_payload+="\n"+"Review requests pending for more than a day"
 
-
-client.chat_postMessage(channel='#general',text=print_payload)
+for edge in result['data']['search']['edges']:
+    for reviewnode in edge['node']['timelineItems']['nodes']:
+        if ts - float(parser.parse(reviewnode['createdAt']).strftime('%s')) > 86400:
+            print_payload+="\n"+reviewnode['requestedReviewer']['login']+" : "+edge['node']['url']
+print(print_payload)
+client.chat_postMessage(channel='#sandbox',text=print_payload)
